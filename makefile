c48243cca834280d89c874178699ce37d9e26f64
Shaon Islam <sidw8@mail.umsl.edu>
2018-09-26 10:37:58 -0500

    Set up shared memory, need to attach

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 CC = gcc
-CFLAGS = -Wall -ansi
+CFLAGS = -Wall 
 TARGET = master
 OBJ = master.o 
 
--- master.c
+++ master.c
@@ -12,14 +12,13 @@ master.c
 #include <unistd.h>
 #include <sys/types.h>
 #include <getopt.h>
-
-/*
 #include <sys/ipc.h>
 #include <sys/shm.h>
-*/
+
+
 /*
 1) allocate shared memory, use it, the deallocate it 
-	-chech all possible errors
+	-check all possible errors
 2) master: fork off and exec one child and have child attach to shared memory
 3) 
 */
@@ -29,35 +28,47 @@ master.c
 int main (int argc, char *argv[])
 {
 
-		int option;
-/*
+	int option;
 
-		int shm_id;
-		key_t key;
-		char *shm;
 
-		key = 3456;
-		shm_id = shmget(key, sizeof(int)*2, IPC_CREAT | 0666);
+	int shm_id;
+	key_t key;
+	char *shm;
 
-		if (shm_id == -1)
-		{
-			perror("shmget");
-			exit(1)
-		}
+	key = 3456;
+	shm_id = shmget(key, sizeof(int)*2, IPC_CREAT | 0666);
+
+	if (shm_id == -1)
+	{
+		perror("shmget");
+		exit(1);
+	}
 		
-		shm = shmat(shm_id, NULL, 0);
-		if (shm == (char * ) -1);
-		{
-			perror("shmat");
-			exit(1);
-		}
+	shm = shmat(shm_id, NULL, 0);
+	if (shm == (char * ) -1);
+	{
+		perror("shmat");
+		exit(1);
+	}		
 
-*/
+	/* init clock : PUT THE CLOCK IN SHARED MEMORY 
+	int* master_clock[2] = shm; */
 
-		/* init clock 
-		int time[2] = {0,0}; */
+	shmctl(shm_id, IPC_RMID, NULL);		 
 
-	        while((option = getopt(argc, argv, "hpn:")) != -1)
+
+
+	/* Create ONE test worker/kid */
+	pid_t worker_pid = 0;
+	worker_pid = fork();
+	if (worker_pid == -1)
+	{
+		perror("fork ");
+	{
+
+
+
+        while((option = getopt(argc, argv, "hpn:")) != -1)
         {
                 switch (option)
                         {
